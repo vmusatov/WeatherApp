3e8caefc674e7615d5f035010f31583a2cd056bc
vmusatov <kotlancelott@gmail.com>
2022-03-21 17:53:10 +0300

    refactor SettingsFragment, popup menu in temp unit setting

--- app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/settings/SettingsFragment.kt
@@ -1,19 +1,35 @@
 package com.example.weatherapp.ui.settings
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.TextView
+import androidx.annotation.MenuRes
+import androidx.core.content.ContextCompat.getColor
+import androidx.core.view.iterator
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.example.weatherapp.R
 import com.example.weatherapp.databinding.FragmentSettingsBinding
-import com.example.weatherapp.ui.*
+import com.example.weatherapp.model.TempUnit
+import com.example.weatherapp.ui.ToolbarAction
 import com.example.weatherapp.ui.home.HomeViewModel
+import com.example.weatherapp.ui.navigator
+import com.example.weatherapp.ui.viewModelFactory
 
-class SettingsFragment : Fragment() {
+class SettingsFragment : Fragment(), PopupMenu.OnMenuItemClickListener {
+
     private lateinit var binding: FragmentSettingsBinding
-    private val viewModel: HomeViewModel by activityViewModels { viewModelFactory() }
+    private val homeViewModel: HomeViewModel by activityViewModels { viewModelFactory() }
+
+    private lateinit var selectedUnit: TextView
+    private lateinit var tempUnitBlock: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,20 +38,64 @@ class SettingsFragment : Fragment() {
     ): View {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
+        setupFields()
         setupUi()
         setupToolbar()
 
         return binding.root
     }
 
+    private fun setupFields() {
+        selectedUnit = binding.selectedUnit
+        tempUnitBlock = binding.tempUnitBlock
+    }
+
     private fun setupUi() {
-        binding.unit.text = viewModel.getTempUnit().unitName
+        selectedUnit.text = homeViewModel.getTempUnit().unitName
+        tempUnitBlock.setOnClickListener {
+            showPopup(
+                binding.unitTitle,
+                R.menu.change_temp_unit_action,
+                homeViewModel.getTempUnit().unitName
+            )
+        }
+    }
 
-        binding.unitTitle.setOnClickListener {
-            val newTempUnit = viewModel.getTempUnit().invert()
+    private fun showPopup(view: View, @MenuRes menuRes: Int, selectedItem: String? = null) {
+        val popup = PopupMenu(requireContext(), view)
+        val inflater = popup.menuInflater
+        inflater.inflate(menuRes, popup.menu)
 
-            binding.unit.text = newTempUnit.unitName
-            viewModel.saveTempUnit(newTempUnit)
+        if (selectedItem != null) {
+            popup.menu.iterator().forEach {
+                if (it.title == selectedItem) {
+                    val spannable = SpannableString(it.title)
+                    val selectedColor =
+                        ForegroundColorSpan(getColor(requireContext(), R.color.selected))
+
+                    spannable.setSpan(selectedColor, 0, spannable.length, 0)
+                    it.title = spannable
+
+                    return@forEach
+                }
+            }
+        }
+
+        popup.setOnMenuItemClickListener(this)
+        popup.show()
+    }
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.C -> {
+                updateTempUnit(TempUnit.C)
+                true
+            }
+            R.id.F -> {
+                updateTempUnit(TempUnit.F)
+                true
+            }
+            else -> false
         }
     }
 
@@ -47,6 +107,11 @@ class SettingsFragment : Fragment() {
                 onAction = { navigator().goBack() }
             )
         )
+    }
+
+    private fun updateTempUnit(tempUnit: TempUnit) {
+        selectedUnit.text = tempUnit.unitName
+        homeViewModel.saveTempUnit(tempUnit)
     }
 
     companion object {
